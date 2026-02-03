958b989bd8f1a85e72df14d5ced0581f7b81b7fe
James <jmp.point@gmail.com>
2026-02-03 22:41:14 +0000

    Finished interests and footer, completed solo project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="social-links">
+        <a className="social-link" href="#">
+          <i className="fa-brands fa-square-twitter"></i>
+        </a>
+        <a className="social-link" href="#">
+          <i className="fa-brands fa-square-facebook"></i>
+        </a>
+        <a className="social-link" href="#">
+          <i className="fa-brands fa-square-instagram"></i>
+        </a>
+        <a className="social-link" href="#">
+          <i className="fa-brands fa-square-github"></i>
+        </a>
+      </div>
+    </footer>
+  );
+}
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,8 +1,8 @@
 export default function Interests() {
   return (
-    <section className="about-section">
-      <h3 className="section-title">About</h3>
-      <p className="section-text">
+    <section className="interests-section">
+      <h3 className="interests-title">Interests</h3>
+      <p className="interests-text">
         I am a fluffy cat with a particular interest in pointing at coconuts and
         sleeping in VS Code terminals.
       </p>
